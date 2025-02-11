24efa49f9dc39c9f0538b2c73d27691fb7d1f4d1
Samantha Tolentino <s.tolentino0828@gmail.com>
2025-02-11 16:48:33 -0500

    Adding styling

--- client/src/components/SideMenu.jsx
+++ client/src/components/SideMenu.jsx
@@ -1,18 +1,29 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 
+import { useAuthStore } from '../store/authStore'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faRectangleList, faCalendar, faUserCircle } from '@fortawesome/free-regular-svg-icons'
+import { faArrowRightFromBracket } from '@fortawesome/free-solid-svg-icons'
 import stickyNote from '../assets/img/sticky-note.png'
-import taskMenuIcon from '../assets/img/task-menu-icon.svg'
 
 const SideMenu = () => {
+  const location = useLocation()
+  const { logout } = useAuthStore()
+
+  const handleLogout = () => {
+    logout();
+  }
+
   return (
-    <div className='bg-white flex flex-col justify-between items-center px-8 py-10 shadow-lg'>
+    <div className='bg-white flex flex-col justify-between items-center p-8 shadow-lg'>
         <img src={stickyNote} alt="sticky-note.png" className='w-10' />
-        <div className='flex flex-col'>
-            <Link>lik</Link>
-            <img src={taskMenuIcon} alt="" className='text-white' />
+        <div className='flex flex-col items-center'>
+            <Link to={'/'}><FontAwesomeIcon icon={faRectangleList} size='xl' className={`${location.pathname == '/' ? 'text-light-purple-1' : 'text-gray-4'} transition hover:text-light-purple-1`} /></Link>
+            <Link to={'/calendar'} className='my-20'><FontAwesomeIcon icon={faCalendar} size='xl' className={`${location.pathname == '/calendar' ? 'text-light-purple-1' : 'text-gray-4'} transition hover:text-light-purple-1`} /></Link>
+            <Link to={'/profile'}><FontAwesomeIcon icon={faUserCircle} size='xl' className={`${location.pathname == '/profile' ? 'text-light-purple-1' : 'text-gray-4'} transition hover:text-light-purple-1`} /></Link>
         </div>
-        <div></div>
+        <Link onClick={handleLogout}><FontAwesomeIcon icon={faArrowRightFromBracket} size='xl' className='text-gray-4 transition hover:text-light-purple-1' /></Link>
     </div>
   )
 }
--- client/src/pages/DashboardPage.jsx
+++ client/src/pages/DashboardPage.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react'
 import { useAuthStore } from '../store/authStore'
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faAngleLeft, faAngleRight } from '@fortawesome/free-solid-svg-icons'
 import SideMenu from '../components/SideMenu';
 
 const DashboardPage = () => {
@@ -14,10 +16,16 @@ const DashboardPage = () => {
   const [taskDeadline, setTaskDeadline] = useState('')
 
   const { user, logout, addCategory, addTask } = useAuthStore();
-
-  const handleLogout = () => {
-    logout();
+  const currentDate = new Date()
+  const options = {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
   }
+
+  // const handleLogout = () => {
+  //   logout();
+  // }
   
   const handleCategorySubmit = async (e) => {
     e.preventDefault()
@@ -32,48 +40,59 @@ const DashboardPage = () => {
   return (
     <div className='bg-light-purple-2 h-screen flex'>
       <SideMenu />
-      <div>
-        <button onClick={() => setTaskFormOpen(true)} className='bg-light-purple-1'>Add Task</button>
-        {
-          taskFormOpen ? 
-            <form onSubmit={handleTaskSubmit} className='bg-white p-2 flex flex-col'>
-              <input onChange={(e) => setTaskName(e.target.value)} value={taskName} type="text" placeholder='Title' className='border' />
-              <textarea onChange={(e) => setTaskDescription(e.target.value)} value={taskDescription} placeholder='Description' className='border'></textarea>
-              <select onChange={(e) => setTaskCategory(e.target.value)} value={taskCategory} className='border'>
-                {
-                  user.categories.map((category, index) => 
-                    <option key={index}>{category.name}</option>
-                  )
-                }
-              </select>
-              <input onChange={(e) => setTaskDeadline(e.target.value)} value={taskDeadline} type="date" className='border' />
-              <button type="submit">Submit</button>
-            </form>
-          :
-            <></>
-        }
+      <div className='py-8 px-12 w-full'>
+        <div className='flex items-center justify-between border-b-2 border-light-purple-4 pb-8'>
+          <div className='flex items-center'>
+            <FontAwesomeIcon className='text-gray-5' icon={faAngleLeft} size='lg' />
+            <div className='mx-8 text-xl'>{currentDate.toLocaleDateString(undefined, options)}</div>
+            <FontAwesomeIcon className='text-gray-5' icon={faAngleRight} size='lg' />
+          </div>
+          <div>
+            <button onClick={() => setTaskFormOpen(true)} className='bg-light-purple-5 text-dark-purple-1 py-2 px-6 rounded-full'>Add Task</button>
+            {
+              taskFormOpen ? 
+                <form onSubmit={handleTaskSubmit} className='bg-white p-2 flex flex-col'>
+                  <input onChange={(e) => setTaskName(e.target.value)} value={taskName} type="text" placeholder='Title' className='border' />
+                  <textarea onChange={(e) => setTaskDescription(e.target.value)} value={taskDescription} placeholder='Description' className='border'></textarea>
+                  <select onChange={(e) => setTaskCategory(e.target.value)} value={taskCategory} className='border'>
+                    {
+                      user.categories.map((category, index) => 
+                        <option key={index}>{category.name}</option>
+                      )
+                    }
+                  </select>
+                  <input onChange={(e) => setTaskDeadline(e.target.value)} value={taskDeadline} type="date" className='border' />
+                  <button type="submit">Submit</button>
+                </form>
+              :
+                <></>
+            }
 
-        <button onClick={() => setCategoryFormOpen(true)} className='bg-light-purple-1'>Add Category</button>
-        {
-          categoryFormOpen ? 
-            <form onSubmit={handleCategorySubmit} className='bg-white p-2 flex flex-col'>
-              <input onChange={(e) => setCategoryName(e.target.value)} value={categoryName} type="text" placeholder='Category name' className='border' />
-              <select onChange={(e) => setCategoryColor(e.target.value)} defaultValue={'default'} className='border'>
-                <option value="default" disabled>Select color</option>
-                <option value="red">Red</option>
-                <option value="orange">Orange</option>
-                <option value="yellow">Yellow</option>
-                <option value="green">Green</option>
-                <option value="blue">Blue</option>
-                <option value="purple">Purple</option>
-              </select>
-              <button type='submit'>Submit</button>
-            </form>
-          :
-            <></>
-        }
+            <button onClick={() => setCategoryFormOpen(true)} className='bg-light-purple-1'>Add Category</button>
+            {
+              categoryFormOpen ? 
+                <form onSubmit={handleCategorySubmit} className='bg-white p-2 flex flex-col'>
+                  <input onChange={(e) => setCategoryName(e.target.value)} value={categoryName} type="text" placeholder='Category name' className='border' />
+                  <select onChange={(e) => setCategoryColor(e.target.value)} defaultValue={'default'} className='border'>
+                    <option value="default" disabled>Select color</option>
+                    <option value="red">Red</option>
+                    <option value="orange">Orange</option>
+                    <option value="yellow">Yellow</option>
+                    <option value="green">Green</option>
+                    <option value="blue">Blue</option>
+                    <option value="purple">Purple</option>
+                  </select>
+                  <button type='submit'>Submit</button>
+                </form>
+              :
+                <></>
+            }
+          </div>
+        </div>
+
       </div>
-      <button className='bg-red-400' onClick={handleLogout}>Logout</button>
+      
+      {/* <button className='bg-red-400' onClick={handleLogout}>Logout</button> */}
     </div>
   )
 }
