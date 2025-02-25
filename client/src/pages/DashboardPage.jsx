da57fcf78615f0c8e6e90da49d6c905e43191c42
Samantha Tolentino <S.Tolentino0828@gmail.com>
2025-02-25 14:09:36 -0500

    Menu updated and scrolling per category added

--- client/src/pages/DashboardPage.jsx
+++ client/src/pages/DashboardPage.jsx
@@ -3,7 +3,7 @@ import { useAuthStore } from '../store/authStore'
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faAngleLeft, faAngleRight, faPlus, faXmark, faCheckCircle, faEllipsis } from '@fortawesome/free-solid-svg-icons'
-import SideMenu from '../components/SideMenu';
+import Menu from '../components/Menu';
 import FormBtn from '../components/FormBtn';
 
 const DashboardPage = () => {
@@ -138,105 +138,105 @@ const DashboardPage = () => {
   }
 
   return (
-    <div className={`bg-gray-9 h-screen flex`}>
+    <div className={`bg-light-purple-2 h-screen flex flex-col`}>
       {taskFormOpen || categoryFormOpen ? <div className='fixed h-screen w-screen bg-white opacity-60'></div> : <></>}
-      <SideMenu />
-      <div className='pt-10 pb-5 px-14 w-full flex flex-col'>
-        <div className='flex items-center justify-between mb-10'>
-          {/* <div className='flex'> */}
-            <div className='flex items-center'>
-              <button onClick={getDateBefore} className='text-xl text-gray-4 transition hover:text-gray-5'>{dayBefore.toLocaleDateString(undefined, options)}</button>
-              <button className='text-3xl mx-8 font-medium flex flex-col text-gray-5'>{currentDate.toLocaleDateString(undefined, options)}
-                <div className='flex justify-center'>
-                  <div className='border-b-3 border-light-purple-1 pb-2 w-1/3'></div>
+      <Menu />
+      <div className='flex items-center justify-between px-12 py-5 mb-5 border-gray-8'>
+        {/* <div className='flex'> */}
+          <div className='flex items-center'>
+            <button onClick={getDateBefore} className='text-xl text-gray-4 transition hover:text-gray-5'>{dayBefore.toLocaleDateString(undefined, options)}</button>
+            <button className='text-3xl mx-8 font-medium flex flex-col text-gray-5'>{currentDate.toLocaleDateString(undefined, options)}
+              <div className='flex justify-center'>
+                <div className='border-b-3 border-light-purple-1 pb-2 w-1/3'></div>
+              </div>
+            </button>
+            <button onClick={getDateAfter} className='text-xl text-gray-4 hover:text-gray-5'>{dayAfter.toLocaleDateString(undefined, options)}</button>
+          </div>
+          <div className='flex items-center'>
+            <button onClick={() => setTaskFormOpen(true)} className='font-medium bg-light-purple-1 text-white py-3 px-5 rounded-md shadow-md flex items-center transition hover:bg-dark-purple-2'><FontAwesomeIcon className='mr-3' icon={faPlus} size='sm' />
+            Task</button>
+            <button onClick={() => setCategoryFormOpen(true)} className='font-medium bg-light-purple-1 text-white py-3 px-5 rounded-md shadow-md flex items-center transition hover:bg-dark-purple-2 ml-3'><FontAwesomeIcon className='mr-4' icon={faPlus} size='sm' />
+            Category</button>
+            {
+              taskFormOpen ? 
+                <div className='fixed top-0 left-0 h-screen w-screen flex justify-center items-center z-10'>
+                  <div className='bg-white rounded-b-md rounded-t-sm overflow-clip w-1/3 shadow-xl shadow-light-purple-2'>
+                    <div className='border-t-4 border-light-purple-1 bg-light-purple-2 py-4 px-4 text-light-purple-1 flex items-center justify-between'>
+                      <div>Add new task</div>
+                      <button onClick={() => setTaskFormOpen(false)}><FontAwesomeIcon className='text-gray-4 transition hover:text-light-purple-1' icon={faXmark} size='lg' /></button>
+                    </div>
+                    <form onSubmit={handleTaskSubmit} className='py-6 px-5 flex flex-col text-gray-5'>
+                      <label className='mb-2'>Title</label>
+                      <input onChange={(e) => setTaskName(e.target.value)} value={taskName} type="text" className='border-1 border-gray-3 rounded-md py-2 px-3 mb-4' />
+                      <label className='mb-2'>Description</label>
+                      {/* Ensure this could be scrolled on screens with less height */}
+                      <textarea onChange={(e) => setTaskDescription(e.target.value)} value={taskDescription} className='border-1 border-gray-3 rounded-md py-2 px-3 mb-4'></textarea>
+                      <div className='flex items-center mb-8'>
+                        <div className='flex flex-col w-1/2 mr-2'>
+                          <label className='mb-2'>Category</label>
+                          <select onChange={(e) => setTaskCategory(e.target.value)} defaultValue={'default'} className='border-1 border-gray-3 rounded-md p-3'>
+                          `<option value="default" disabled></option>
+                            {
+                              user.categories.map((category, index) => 
+                                <option key={index}>{category.name}</option>
+                              )
+                            }
+                          </select>
+                        </div>
+                        <div className='flex flex-col w-1/2 ml-2'>
+                          <label className='mb-2'>Due Date & Time</label>
+                          <input onChange={(e) => setTaskDeadline(e.target.value)} value={taskDeadline} type="datetime-local" className='border-1 border-gray-3 rounded-md py-2 px-3' />
+                        </div>
+                      </div>
+                      <div className='flex justify-end font-medium'>
+                        <button className='bg-light-purple-1 text-white rounded-full px-5 py-2 w-1/3 shadow-md transition hover:bg-dark-purple-2' type='submit'>Save</button>
+                      </div>
+                    </form>
+                  </div>
                 </div>
-              </button>
-              <button onClick={getDateAfter} className='text-xl text-gray-4 hover:text-gray-5'>{dayAfter.toLocaleDateString(undefined, options)}</button>
-            </div>
-            <div className='flex items-center'>
-              <button onClick={() => setTaskFormOpen(true)} className='font-medium bg-light-purple-1 text-white py-3 px-5 rounded-md shadow-md flex items-center transition hover:bg-dark-purple-2'><FontAwesomeIcon className='mr-3' icon={faPlus} size='sm' />
-              Task</button>
-              <button onClick={() => setCategoryFormOpen(true)} className='font-medium bg-light-purple-1 text-white py-3 px-5 rounded-md shadow-md flex items-center transition hover:bg-dark-purple-2 ml-3'><FontAwesomeIcon className='mr-4' icon={faPlus} size='sm' />
-              Category</button>
-              {
-                taskFormOpen ? 
-                  <div className='fixed top-0 left-0 h-screen w-screen flex justify-center items-center z-10'>
-                    <div className='bg-white rounded-b-md rounded-t-sm overflow-clip w-1/3 shadow-xl shadow-light-purple-2'>
-                      <div className='border-t-4 border-light-purple-1 bg-light-purple-2 py-4 px-4 text-light-purple-1 flex items-center justify-between'>
-                        <div>Add new task</div>
-                        <button onClick={() => setTaskFormOpen(false)}><FontAwesomeIcon className='text-gray-4 transition hover:text-light-purple-1' icon={faXmark} size='lg' /></button>
-                      </div>
-                      <form onSubmit={handleTaskSubmit} className='py-6 px-5 flex flex-col text-gray-5'>
-                        <label className='mb-2'>Title</label>
-                        <input onChange={(e) => setTaskName(e.target.value)} value={taskName} type="text" className='border-1 border-gray-3 rounded-md py-2 px-3 mb-4' />
-                        <label className='mb-2'>Description</label>
-                        {/* Ensure this could be scrolled on screens with less height */}
-                        <textarea onChange={(e) => setTaskDescription(e.target.value)} value={taskDescription} className='border-1 border-gray-3 rounded-md py-2 px-3 mb-4'></textarea>
-                        <div className='flex items-center mb-8'>
-                          <div className='flex flex-col w-1/2 mr-2'>
-                            <label className='mb-2'>Category</label>
-                            <select onChange={(e) => setTaskCategory(e.target.value)} defaultValue={'default'} className='border-1 border-gray-3 rounded-md p-3'>
-                            `<option value="default" disabled></option>
-                              {
-                                user.categories.map((category, index) => 
-                                  <option key={index}>{category.name}</option>
-                                )
-                              }
-                            </select>
-                          </div>
-                          <div className='flex flex-col w-1/2 ml-2'>
-                            <label className='mb-2'>Due Date & Time</label>
-                            <input onChange={(e) => setTaskDeadline(e.target.value)} value={taskDeadline} type="datetime-local" className='border-1 border-gray-3 rounded-md py-2 px-3' />
-                          </div>
-                        </div>
-                        <div className='flex justify-end font-medium'>
-                          <button className='bg-light-purple-1 text-white rounded-full px-5 py-2 w-1/3 shadow-md transition hover:bg-dark-purple-2' type='submit'>Save</button>
-                        </div>
-                      </form>
+              :
+                <></>
+            }
+            {
+              categoryFormOpen ? 
+                <div className='fixed top-0 left-0 h-screen w-screen flex justify-center items-center z-10'>
+                  <div className='bg-white rounded-b-md rounded-t-sm overflow-clip w-1/3 shadow-xl shadow-light-purple-2'>
+                    <div className='border-t-4 border-light-purple-1 bg-light-purple-2 py-4 px-4 text-light-purple-1 flex items-center justify-between'>
+                      <div>Add new category</div>
+                      <button onClick={() => setCategoryFormOpen(false)}><FontAwesomeIcon className='text-gray-4 transition hover:text-light-purple-1' icon={faXmark} size='lg' /></button>
                     </div>
-                  </div>
-                :
-                  <></>
-              }
-              {
-                categoryFormOpen ? 
-                  <div className='fixed top-0 left-0 h-screen w-screen flex justify-center items-center z-10'>
-                    <div className='bg-white rounded-b-md rounded-t-sm overflow-clip w-1/3 shadow-xl shadow-light-purple-2'>
-                      <div className='border-t-4 border-light-purple-1 bg-light-purple-2 py-4 px-4 text-light-purple-1 flex items-center justify-between'>
-                        <div>Add new category</div>
-                        <button onClick={() => setCategoryFormOpen(false)}><FontAwesomeIcon className='text-gray-4 transition hover:text-light-purple-1' icon={faXmark} size='lg' /></button>
+                    <form onSubmit={handleCategorySubmit} className='py-6 px-5 flex flex-col text-gray-5'>
+                      <label className='mb-2'>Name</label>
+                      <input onChange={(e) => setCategoryName(e.target.value)} value={categoryName} type="text" className='border-1 border-gray-3 rounded-md py-2 px-3 mb-4' />
+                      <label className='mb-2'>Color</label>
+                      <select onChange={(e) => setCategoryColor(e.target.value)} defaultValue={'default'} className={`text-${categoryColor}-category border-1 border-gray-3 rounded-md py-2 px-3 mb-8 font-semibold`}>
+                        <option value="default" disabled></option>
+                        <option value="red" className='font-semibold text-red-category'>Red</option>
+                        <option value="orange" className='font-semibold text-orange-category'>Orange</option>
+                        <option value="yellow" className='font-semibold text-yellow-category'>Yellow</option>
+                        <option value="green" className='font-semibold text-green-category'>Green</option>
+                        <option value="blue" className='font-semibold text-blue-category'>Blue</option>
+                        <option value="purple" className='font-semibold text-purple-category'>Purple</option>
+                        <option value="pink" className='font-semibold text-pink-category'>Pink</option>
+                      </select>
+                      <div className='flex justify-end font-medium'>
+                        <button className='bg-light-purple-1 text-white rounded-full px-5 py-2 w-1/3 shadow-md transition hover:bg-dark-purple-2' type='submit'>Save</button>
                       </div>
-                      <form onSubmit={handleCategorySubmit} className='py-6 px-5 flex flex-col text-gray-5'>
-                        <label className='mb-2'>Name</label>
-                        <input onChange={(e) => setCategoryName(e.target.value)} value={categoryName} type="text" className='border-1 border-gray-3 rounded-md py-2 px-3 mb-4' />
-                        <label className='mb-2'>Color</label>
-                        <select onChange={(e) => setCategoryColor(e.target.value)} defaultValue={'default'} className={`text-${categoryColor}-category border-1 border-gray-3 rounded-md py-2 px-3 mb-8 font-semibold`}>
-                          <option value="default" disabled></option>
-                          <option value="red" className='font-semibold text-red-category'>Red</option>
-                          <option value="orange" className='font-semibold text-orange-category'>Orange</option>
-                          <option value="yellow" className='font-semibold text-yellow-category'>Yellow</option>
-                          <option value="green" className='font-semibold text-green-category'>Green</option>
-                          <option value="blue" className='font-semibold text-blue-category'>Blue</option>
-                          <option value="purple" className='font-semibold text-purple-category'>Purple</option>
-                          <option value="pink" className='font-semibold text-pink-category'>Pink</option>
-                        </select>
-                        <div className='flex justify-end font-medium'>
-                          <button className='bg-light-purple-1 text-white rounded-full px-5 py-2 w-1/3 shadow-md transition hover:bg-dark-purple-2' type='submit'>Save</button>
-                        </div>
-                      </form>
-                    </div>
+                    </form>
                   </div>
-                :
-                  <></>
-              }
-            </div>
-          {/* </div> */}
-        </div>
-        <div className='relative w-full grow overflow-x-auto'>
-          <div className='tasks-container w-full h-full absolute top-0 left-0'>
+                </div>
+              :
+                <></>
+            }
+          </div>
+        {/* </div> */}
+      </div>
+      <div className='w-full flex flex-col grow min-h-0'>
+        <div className='flex flex-col grow w-full overflow-x-auto'>
+          <div className='tasks-container w-full px-12'>
             {
               tasks.map((taskCategory, index) => {
-                return <div key={index} className={`${!taskCategory.length ? 'hidden' : ''} h-fit py-6 px-5`}>{taskCategory.map((task, index) => {
+                return <div key={index} className={`${!taskCategory.length ? 'hidden' : ''} min-h-0 px-3 overflow-y-auto`}>{taskCategory.map((task, index) => {
                   let categoryColor = user.categories.find((taskCat) => taskCat.name == task.category)
 
                   return <div key={index}>
