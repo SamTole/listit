6204059956f7d8566889e1086ebfab66fa52c1c0
Samantha Tolentino <S.Tolentino0828@gmail.com>
2025-03-06 12:25:24 -0500

    Adding mobile responsiveness

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -28,23 +28,23 @@ const LoginPage = () => {
   }
 
   return (
-    <div className='grid grid-cols-5 h-screen relative'>
-      <div className='absolute py-6 px-8'>
+    <div className='md:grid grid-cols-5 flex flex-col h-screen relative'>
+      <div className='md:absolute md:flex-none col-span-5 flex justify-start items-start w-full top-0 left-0 py-6 px-8'>
         <Logo />
       </div>
-      <div className='col-span-3 flex flex-col items-center justify-center text-center'>
-        <motion.div className='w-3/5'
+      <div className='col-span-3 flex flex-col items-center justify-center text-center w-full md:max-lg:pt-28 md:max-lg:pb-20'>
+        <motion.div className='lg:w-3/5 w-full lg:px-0 md:max-lg:px-20 px-10'
           initial={{ opacity: 0, y: 30}}
           animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 0.45, ease: 'easeInOut', delay: 0.15}}
         >
-          <div className='text-5xl font-medium mb-5'>Welcome Back!</div>
+          <div className='lg:text-5xl text-4xl font-medium lg:mb-5 mb-3'>Welcome Back!</div>
           <div className='text-lg text-gray-1 font-medium mb-10'>Please enter your credentials to sign in.</div>
-          <form onSubmit={handleLogin} className='flex flex-col items-center w-full mb-8'>
-            <motion.div className={ `w-full bg-gray-8 px-6 flex items-center rounded-full text-gray-4 shadow mb-6 max-w-lg`}>
+          <form onSubmit={handleLogin} className='flex flex-col items-center w-full mb-10'>
+            <div className={ `w-full bg-gray-8 px-6 flex items-center rounded-full text-gray-4 shadow mb-6 max-w-lg`}>
               <FontAwesomeIcon icon={faEnvelope} />
               <InputField onChange={(e) => setEmail(e.target.value)} name='email' value={email} type='email' placeholder='Email' />
-            </motion.div>
+            </div>
             <div className={`w-full bg-gray-8 px-6 flex items-center rounded-full text-gray-4 shadow mb-6 max-w-lg`}>
               <FontAwesomeIcon icon={faLock} />
               <InputField onChange={(e) => setPassword(e.target.value)} name='password' value={password} type='password' placeholder='Password' classes='mb-10 max-w-lg' />
@@ -54,7 +54,7 @@ const LoginPage = () => {
           <div className='text-gray-1'>Don't have an account? <Link to={'/signup'} className='text-light-purple-1 transition hover:text-dark-purple-2 font-semibold border-b-2 border-transparent hover:border-light-purple-1'>Sign up here</Link></div>
         </motion.div>
       </div>
-      <motion.div className='col-span-2 bg-light-purple-2 flex items-center justify-center'
+      <motion.div className='col-span-2 bg-light-purple-2 md:flex hidden items-center justify-center'
         initial={{ opacity: 0, y: -50}}
         animate={{ opacity: 1, y: 0 }}
         transition={{ duration: 0.45, ease: 'easeOut', delay: 0.15}}
